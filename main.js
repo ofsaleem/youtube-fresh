f3b33cb0de39a1b4cd3f76d7ce8dcecfbb7e3794
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-20 21:18:28 -0500

    more tutorial

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ function createWindow () {
 
 app.whenReady().then(() => {
     createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 app.on('window-all-closed', function () {
