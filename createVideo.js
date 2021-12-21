3a80cc0a68bfe90c6b23133eda62d5c38c011389
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-20 21:37:04 -0500

    beginning process of porting code

---
+++ createVideo.js
@@ -0,0 +1,10 @@
+function getFfmpeg() {
+    const ffbinaries = require('ffbinaries-extra');
+    let platform = ffbinaries.detectPlatform();
+    ffbinaries.downloadFiles(['ffmpeg', 'ffprobe'], {platform: platform}, function() {
+        output.innerHTML += '\nDownloaded ffmpeg and ffprobe for platform ' + platform;
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+    });
+    process.env.FFMPEG_PATH = '.\\ffmpeg.exe';
+    process.env.FFPROBE_PATH = '.\\ffprobe.exe';
+}
--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const path = require('path')
 
 function createWindow () {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600,
+      width: 1280,
+      height: 720,
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
     })
     win.loadFile('index.html')
+    // Open the DevTools.
+    win.webContents.openDevTools();
   }
 
 app.whenReady().then(() => {
