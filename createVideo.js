1f506ca4440a2f68965c144e56d0bddfa8171b02
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-25 01:47:51 -0500

    removing env vars for now

--- createVideo.js
+++ createVideo.js
@@ -2,6 +2,4 @@ async function getFfmpeg() {
     let platform = await ffbinaries.downloadFFBinaries();
     output.innerHTML += '\nDownloaded ffmpeg and ffprobe for platform ' + platform;
     output.scrollTop = output.scrollHeight - output.clientHeight;
-    process.env.FFMPEG_PATH = '.\\ffmpeg.exe';
-    process.env.FFPROBE_PATH = '.\\ffprobe.exe';
 }
