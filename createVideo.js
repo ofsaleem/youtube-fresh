92e8f88e29e8208fa315d56baf6579303c8eeb66
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-31 20:42:17 -0500

    moved getimagewidth to createvideo

--- createVideo.js
+++ createVideo.js
@@ -7,12 +7,31 @@ async function getFfmpeg() {
 function createVideo() {
     let mp3path = document.getElementById("mp3file").files[0].path;
     let imagepath = document.getElementById("imagefile").files[0].path;
-    let output = document.getElementById('output');
     let visualizer = document.getElementById('visualizer');
     if (visualizer.value == 'vectorscope') {
-        ffmpeg.renderWithVectorscope(mp3path, imagepath, output);
+        ffmpeg.renderWithVectorscope(mp3path, imagepath);
     }
     else {
-        renderWithWaveforms(mp3path, imagepath, output);
+        let fixedImageWidth = getImageWidth(imagepath);
+        renderWithWaveforms(mp3path, imagepath, fixedImageWidth);
     }
+}
+
+function getImageWidth(input) {
+    let fixedimagewidth = 600;
+    let fr = new FileReader;
+    fr.onload = () => {
+        let img = new Image;
+        img.onload = () => {
+            let imagewidth = +img.width;
+            let imageheight = +img.height;
+            let ar = imagewidth * 1.0 / imageheight;
+            ar = +(parseFloat(ar.toFixed(2)));
+            fixedimagewidth = 600 * ar;
+            fixedimagewidth = +fixedimagewidth.toFixed(0);
+            return fixedimagewidth;
+        };
+        img.src = fr.result;
+    };
+    fr.readAsDataURL(input.files[0]);
 }
