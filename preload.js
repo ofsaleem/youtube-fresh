afcfc67cec452ae524ca444d30c74c960ab5b1d7
Omar Saleem <ofsaleem@users.noreply.github.com>
2023-03-23 22:22:40 -0400

    updating from local

--- preload.js
+++ preload.js
@@ -33,6 +33,26 @@ contextBridge.exposeInMainWorld(
   }
 );
 
+contextBridge.exposeInMainWorld(
+  "api", {
+      send: (channel, data) => {
+          // whitelist channels
+          let validChannels = ["ffbinaries", "ffmpeg"];
+          if (validChannels.includes(channel)) {
+              ipcRenderer.send(channel, data);
+          }
+      },
+      receive: (channel, func) => {
+          let validChannels = ["fromMain"];
+          if (validChannels.includes(channel)) {
+              // Deliberately strip event as it includes `sender` 
+              ipcRenderer.on(channel, (event, ...args) => func(...args));
+          }
+      }
+  }
+);
+
+
 ipcRenderer.on('ffmpeg-encoding-start', (event, commandLine) => {
   let output = document.getElementById('output');
   output.innerHTML += 'Spawned Ffmpeg with command: ' + commandLine + '\n' +
